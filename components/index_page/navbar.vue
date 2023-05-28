43e14b77c12049cb1488426947d341c1b4674fd4
Kai Reichart <kai@reichart.dev>
2023-05-28 14:40:33 +0200

    Navbar

--- components/index_page/navbar.vue
+++ components/index_page/navbar.vue
@@ -1,59 +1,68 @@
 <template>
 
-    <div class="">
-        <div class="nav absolute w-full top-0 left-0 h-fit py-10" :class="{'menuOpen': menuOpen}">
-            <div class="container flex justify-between">
-                <nuxt-link to="/" class="">
-                    <nuxt-img class="logo" src="/images/logo.svg"/>
-                </nuxt-link>
+    <div>
 
-                <div class="flex items-center">
-                    <nuxt-link to="https://talks.adoptingbitcoin.org" class="underline mb-2 mr-12 hidden md:block"
-                               target="_blank">Call for participation
+        <Head>
+            <Body :class="{'overflow-hidden': menuOpen}"></Body>
+        </Head>
+
+        <div class="nav absolute w-full top-0 left-0 py-10 overflow-y-scroll"
+             :class="{'menuOpen fixed h-full pb-0 nav-overlay': menuOpen}">
+            <div class="z-40">
+                <div class="container flex justify-between">
+                    <nuxt-link to="/" class="">
+                        <nuxt-img class="logo" src="/images/logo.svg"/>
                     </nuxt-link>
-                    <a v-on:click="menuOpen = !menuOpen" class="h-18 w-18 cursor-pointer">
-                        <div class="m-auto" :class="menuOpen ? 'hidden' : 'visible'">
-                            <span class="material-symbols-outlined text-5xl">drag_handle</span>
-                        </div>
 
-                        <div class="m-auto" :class="menuOpen ? 'visible' : 'hidden'">
-                            <span class="material-symbols-outlined text-5xl">close</span>
-                        </div>
-                    </a>
+                    <div class="flex items-center">
+                        <nuxt-link to="https://talks.adoptingbitcoin.org" class="underline mb-2 mr-12 hidden md:block"
+                                   target="_blank">Call for participation
+                        </nuxt-link>
+                        <a v-on:click="menuOpen = !menuOpen" class="h-18 w-18 cursor-pointer">
+                            <div class="m-auto" :class="menuOpen ? 'hidden' : 'visible'">
+                                <span class="material-symbols-outlined text-5xl">drag_handle</span>
+                            </div>
+
+                            <div class="m-auto" :class="menuOpen ? 'visible' : 'hidden'">
+                                <span class="material-symbols-outlined text-5xl">close</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="w-full md:hidden flex justify-between">
+                    <div class="mx-auto mt-5">
+                        <nuxt-link to="https://talks.adoptingbitcoin.org" class="underline" target="_blank">Call for
+                            participation
+                        </nuxt-link>
+                    </div>
+
                 </div>
             </div>
-            <div class="w-full md:hidden flex justify-between">
-                <div class="mx-auto mt-5">
-                    <nuxt-link to="https://talks.adoptingbitcoin.org" class="underline" target="_blank">Call for
-                        participation
-                    </nuxt-link>
+
+            <div class="w-full h-full py-16" v-if="menuOpen">
+                <div class="bg-clouds w-full top-0 left-0 h-fit absolute -z-20"></div>
+                <div class="container">
+                    <div class="flex flex-col menu-links text-right">
+                        <nuxt-link to="/#venue" v-on:click="menuOpen = false">venue</nuxt-link>
+                        <nuxt-link to="/#speakers" v-on:click="menuOpen = false">speakers</nuxt-link>
+                        <nuxt-link to="/#ticket" v-on:click="menuOpen = false">ticket</nuxt-link>
+                        <nuxt-link to="/#sponsors" v-on:click="menuOpen = false">sponsors</nuxt-link>
+                        <nuxt-link to="/#partners" v-on:click="menuOpen = false">partners</nuxt-link>
+                        <nuxt-link to="/#team" v-on:click="menuOpen = false">team</nuxt-link>
+
+                        <nuxt-link to="https://www.bitcoinerevents.com/bitcoin-month/" target="_blank">Bitcoin Month
+                        </nuxt-link>
+                        <nuxt-link to="https://talks.adoptingbitcoin.org" target="_blank">Call for Participation
+                        </nuxt-link>
+                        <nuxt-link to="/faq">FAQ</nuxt-link>
+                    </div>
+
                 </div>
 
             </div>
 
         </div>
 
-        <div class="fixed w-full h-full top-0 left-0 nav-overlay" v-if="menuOpen">
-            <div class="bg-clouds w-full top-0 left-0 h-full absolute bg-blend-lighten z-10"></div>
-            <div class="container mt-44 md:mt-52">
-
-                <div class="flex flex-col menu-links text-right">
-                    <nuxt-link to="/#venue" v-on:click="menuOpen = false">venue</nuxt-link>
-                    <nuxt-link to="/#speakers" v-on:click="menuOpen = false">speakers</nuxt-link>
-                    <nuxt-link to="/#ticket" v-on:click="menuOpen = false">ticket</nuxt-link>
-                    <nuxt-link to="/#sponsors" v-on:click="menuOpen = false">sponsors</nuxt-link>
-                    <nuxt-link to="/#partners" v-on:click="menuOpen = false">partners</nuxt-link>
-                    <nuxt-link to="/#team" v-on:click="menuOpen = false">team</nuxt-link>
-
-                    <nuxt-link to="https://www.bitcoinerevents.com/bitcoin-month/" target="_blank">Bitcoin Month
-                    </nuxt-link>
-                    <nuxt-link to="https://talks.adoptingbitcoin.org" target="_blank">Call for Participation</nuxt-link>
-                    <nuxt-link to="/faq">FAQ</nuxt-link>
-                </div>
-
-            </div>
-
-        </div>
 
     </div>
 
@@ -64,6 +73,14 @@ export default {
     data() {
         return {
             menuOpen: false
+        }
+    },
+
+    head() {
+        return {
+            bodyAttrs: {
+                class: this.menuOpen ? 'overflow-hidden' : ''
+            }
         }
     },
 }
