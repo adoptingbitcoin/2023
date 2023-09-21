0dde441604b5e039abfd61c291a79336d4ff9059
Kai Reichart <kai@reichart.dev>
2023-09-21 17:03:11 +0200

    added footer and long text

--- components/footer_component.vue
+++ components/footer_component.vue
@@ -1,59 +1,68 @@
 <template>
-    <div class="">
-        <GetInTouch/>
+  <div class="">
+    <GetInTouch/>
 
-        <div class="component slim-component">
+    <div class="component slim-component">
 
-            <StraightBanner topImage="/images/section-heading--footer.svg"/>
+      <StraightBanner topImage="/images/section-heading--footer.svg"/>
 
-            <div class="container flex-col md:flex-row flex w-full justify-between items-center [&>*]:m-5">
-                <div class="uppercase">
-                    <span>Adopting Bitcoin    ·    The Crowne Plaza </span> <br>
-                    <span>89 Avenida Norte    ·    San Salvador</span>
-                </div>
+      <div class="container flex-col md:flex-row flex w-full justify-between items-center [&>*]:m-5">
+        <div class="uppercase">
+          <span>Adopting Bitcoin    ·    The Crowne Plaza </span> <br>
+          <span>89 Avenida Norte    ·    San Salvador</span>
+        </div>
 
-                <TicketPurchase class="mx-auto"/>
+        <TicketPurchase class="mx-auto"/>
 
-                <div class="">
-                    <div class="flex">
+        <div class="">
+          <div class="flex">
                     <span class="uppercase mr-5">
                         A conference <br>
                         hosted by
                     </span>
-                        <nuxt-link to="https://galoy.io" target="_blank">
-                            <nuxt-img class="h-full w-auto object-contain" loading="lazy" src="/images/galoy_logo.png"
-                                      alt=""/>
-                        </nuxt-link>
-                    </div>
-                </div>
+            <div class="">
+              <nuxt-link to="https://galoy.io" target="_blank" class="cursor-pointer">
+                <nuxt-img class="h-10 w-auto object-contain" loading="lazy" src="/images/sponsors/galoy.svg"
+                          alt=""/>
+              </nuxt-link>
+              <div class="h-5"></div>
+              <nuxt-link class="cursor-pointer" to="https://bitfinex.com" target="_blank">
+                <nuxt-img class="h-5 w-auto object-contain" loading="lazy"
+                          src="/images/sponsors/Bitfinex_new-logo_white.svg"
+                          alt=""/>
+              </nuxt-link>
             </div>
 
-            <div class="container flex-col md:flex-row flex w-full justify-between items-center mt-20 [&>*]:m-5">
-                <div class="uppercase w-fit">
-                    <a target="_blank" href="https://t.me/AdoptingBTC">/telegram</a> &nbsp;
-                    <a target="_blank" href="https://www.linkedin.com/company/76150672">/linkedin</a> &nbsp;
-                    <a target="_blank" href="https://twitter.com/AdoptingBTC">/twitter</a> &nbsp;
-                    <a target="_blank"
-                       href="https://snort.social/p/npub1ad0ptuzte83alpkpqqctvewlaqj5zq23c83m82xw8apg4t6zlyns6xng2c">/nostr</a>
-                    &nbsp;
-                    <a target="_blank" href=" https://github.com/nuraelb/AdoptingBitcoin">/GitHub</a> &nbsp;
-                    <br>
-                    <a href="mailto:info@adoptingbitcoin.org">email if you need / info@adoptingbitcoin.org</a>
+          </div>
+        </div>
+      </div>
 
-                </div>
-
-                <br>
-
-                <div class="uppercase text-left md:text-right w-fit">
-                    <span>© adopting bitcoin 2023</span>
-                    <br>
-                    <span>DESIGNED BY PEAK SHIFT LTD.</span>
-                </div>
-            </div>
-
+      <div class="container flex-col md:flex-row flex w-full justify-between items-center mt-20 [&>*]:m-5">
+        <div class="uppercase w-fit">
+          <a target="_blank" href="https://t.me/AdoptingBTC">/telegram</a> &nbsp;
+          <a target="_blank" href="https://www.linkedin.com/company/76150672">/linkedin</a> &nbsp;
+          <a target="_blank" href="https://twitter.com/AdoptingBTC">/twitter</a> &nbsp;
+          <a target="_blank"
+             href="https://snort.social/p/npub1ad0ptuzte83alpkpqqctvewlaqj5zq23c83m82xw8apg4t6zlyns6xng2c">/nostr</a>
+          &nbsp;
+          <a target="_blank" href=" https://github.com/nuraelb/AdoptingBitcoin">/GitHub</a> &nbsp;
+          <br>
+          <a href="mailto:info@adoptingbitcoin.org">email if you need / info@adoptingbitcoin.org</a>
 
         </div>
+
+        <br>
+
+        <div class="uppercase text-left md:text-right w-fit">
+          <span>© adopting bitcoin 2023</span>
+          <br>
+          <span>DESIGNED BY PEAK SHIFT LTD.</span>
+        </div>
+      </div>
+
+
     </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -75,7 +84,7 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import GetInTouch from "~/components/index_page/GetInTouch.vue";
 
 export default {
-    components: {GetInTouch, FontAwesomeIcon}
+  components: {GetInTouch, FontAwesomeIcon}
 
 }
 </script>
--- content/speakers/PaoloArdoino.md
+++ content/speakers/PaoloArdoino.md
@@ -4,4 +4,45 @@ company: Bitfinex
 img: paolo.jpg
 ---
 
-Paolo Ardoino serves as the Chief Technology Officer of Tether and Bitfinex. Paolo became fascinated with coding when he was 8 years old and ever since, his favourite thing to do has been experimenting with how new technologies can be harnessed to solve some of the world's most pressing problems.
+Paolo Ardoino serves as the Chief Technology Officer of Tether and Bitfinex. Paolo became fascinated with coding when he
+was 8 years old and ever since, his favourite thing to do has been experimenting with how new technologies can be
+harnessed to solve some of the world's most pressing problems.
+
+<br>
+
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+
+<br>
+
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+<br>
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+lorem ipsum dolor sit amet, consectetur adipiscing elit. sed euismod, diam eu aliquam ultricies, quam diam aliquam diam,
+quis ali
+
+<br>
+
+This is the end
